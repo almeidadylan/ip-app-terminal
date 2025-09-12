5609b3b8f104832d77daa298c7463d7131756b3d
almeidadylan <almeidadylan793@gmail.com>
2025-09-12 17:40:50 -0400

    refactor: adicionando outra maneira de conseguir o ip local

--- src/screen/ip.rs
+++ src/screen/ip.rs
@@ -1,3 +1,5 @@
+use std::net::UdpSocket;
+
 use crate::screen::basic_operations::*;
 use crate::screen::read::read_string_data;
 
@@ -5,5 +7,26 @@ pub fn get_my_local_ip() {
     clear_screen();
     let my_local_ip = local_ip_address::local_ip().unwrap();
     println!("Seu ip local é: {}", my_local_ip);
+
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    socket.connect("8.8.8.8:80").unwrap(); // conecta a um servidor externo (Google DNS)
+
+    let local_addr = socket.local_addr().unwrap();
+    println!("Seu ip local é: {}", local_addr.ip());
+
+
+
     let _input = read_string_data();
+
+
+}
+
+fn _get_my_local_ip2() {
+    //clear_screen();
+    // Criar um socket UDP sem realmente enviar nada
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    socket.connect("8.8.8.8:80").unwrap(); // conecta a um servidor externo (Google DNS)
+
+    let local_addr = socket.local_addr().unwrap();
+    println!("Seu ip local é: {}", local_addr.ip());
 }
